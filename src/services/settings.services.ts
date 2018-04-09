a0d964bb29e1afd28153bb85b253b93a33f542bf
Anton <funfun2010@mail.ru>
2018-04-09 15:28:01 +0300

    merg

---
+++ src/services/settings.services.ts
@@ -0,0 +1,23 @@
+import {Injectable} from '@angular/core';
+import {RequestServices} from './request.services';
+
+@Injectable()
+export class SettingsServices {
+  constructor(private request: RequestServices) {}
+
+  getAccount() {
+    return this.request.get('account', true);
+  }
+
+  getLanguages() {
+    return this.request.get('languages', true);
+  }
+
+  save(profile: object) {
+    return this.request.post('account/profile', profile, true);
+  }
+
+  changeEmail(email: object) {
+    return this.request.post('account/email/change', email, true);
+  }
+}
