df470993b1be5baea6218dc0532fea1de655d5c4
Anton Ryzanov <a.ryazanov@encry.ru>
2018-05-15 19:40:21 +0300

    swap in work

--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -102,6 +102,6 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.getCurse();
-    this.navigateToLastUrl();
+    // this.navigateToLastUrl();
   }
 }
--- src/services/swap.services.ts
+++ src/services/swap.services.ts
@@ -20,5 +20,8 @@ export class SwapServices {
     return this._req.getEth('module=account&action=tokenbalance&contractaddress=0xe0c72452740414d861606a44ccd5ea7f96488278&address=0x53a3acb0c205fce16b58c05d145009dc83af0b38' +
       '&tag=latest&apikey=6IUI24CQUWUPAS3N2QYFVT9R6BFY5F1HAN', true)
   }
+  postData(data): Promise<any> {
+    return this._req.post('address/bind', data, true);
+  }
 }
 
