75b267180037f38bf26503effdf9b9982d96be46
gapaus <a.sabirzyanov@encryptotel.com>
2018-05-30 15:15:54 +0300

    fix files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  api_url: 'http://investor.loc/api',
-  site_url: 'http://investor.loc',
+  api_url: 'http://investor-front-dev.encry.ru/api',
+  site_url: 'http://investor-front-dev.encry.ru',
   waves_api_url: 'https://nodes.wavesnodes.com',
   waves_api_dev: 'https://testnode1.wavesnodes.com',
   eth_api_url: 'https://api.etherscan.io/api',
